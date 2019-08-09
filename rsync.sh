2e3b3600f43c0ac8dc99338fc4a89fd288a94f9d
Yuncraft <yuncraft@163.com>
2019-08-09 20:20:16 +0800

    First update First update

---
+++ rsync.sh
@@ -0,0 +1,7 @@
+rsync -avrt rsync://mirrors4.tuna.tsinghua.edu.cn/centos/ /mirror/centos/
+#默认使用清华大学源
+#Default Use Tsinghua University's Source
+#此处为CentOS系统实例
+#Here is an example of the CentOS system
+#/mirror/centos/是本地目录
+#/mirror/centos/ is the local directory
